0d275b8dde3a7b53589365b76474e2dade874f6e
Jobsidney <jobsidney67@gmail.com>
2022-08-09 11:43:35 +0300

    string interpolation

--- ruby/ruby_new.rb
+++ ruby/ruby_new.rb
@@ -12,3 +12,11 @@ def syaKill
     puts 'killing '
 end
 syaKill
+
+
+#STRING INTERPOLATION/ CONCATINATION
+firstName="Jovan"
+lastName="Sidney"
+fullName="#{firstName} #{lastName}"
+puts fullName
+puts "my first name is #{firstName} and  last name is #{lastName}"
